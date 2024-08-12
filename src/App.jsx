dc3a2c6e6284b5143fc5c0b0f161414f8d50a1cb
Rudraksh Girdhar <rudrakshgirdhar9192@gmail.com>
2024-08-12 19:31:54 +0530

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImageUpload from './component/ImageUpload'
+
+const App = () => {
+  return (
+    <div>
+      Hello
+      <ImageUpload/>
+    </div>
+  )
+}
+
+export default App
